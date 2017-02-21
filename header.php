313f9084d335811bc0ccf3ecef2c917009cff15c
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-21 12:00:06 -0700

    test

--- header.php
+++ header.php
@@ -9,13 +9,3 @@
       <img id="logo2" src="images/logo2.jpg" />
       <h1>BSA Quidditch</h1>
     </div>
-    <div id="nav">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="roster.php">Roster</a></li>
-        <li><a href="upcoming.php">Upcoming Tournaments</a></li>
-        <li><a href="videos.php">Videos</a></li>
-        <li><a href="practices.php">Practice Schedule</a></li>
-        <li><a href="announcements.php">Announcements</a></li>
-      </ul>
-    </div>
