a776c8b4fa8eb9587eb19d1d8beda24825d8dd5a
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-21 12:08:13 -0700

    Adding header footer and nav bar to all pages

---
+++ roster.php
@@ -0,0 +1,20 @@
+<?php
+  include('header.php');
+?>
+
+
+  <div id="nav">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a class=active href="roster.php">Roster</a></li>
+      <li><a href="upcoming.php">Upcoming Tournaments</a></li>
+      <li><a href="videos.php">Videos</a></li>
+      <li><a href="practices.php">Practice Schedule</a></li>
+      <li><a href="announcements.php">Announcements</a></li>
+    </ul>
+  </div>
+
+
+<?php
+  include('footer.php');
+?>
