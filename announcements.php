d949c11694af2b9fc425ce1ce3adcaf327511f54
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-22 13:59:00 -0700

    announcements page pt2

--- announcements.php
+++ announcements.php
@@ -16,25 +16,24 @@
 
 <h2> Team Announcements </h2>
 
-<p>This is where announcements will be located.
   <br />
-  <br />
-1. Practice schedule. <br />
-2. Upcoming events. <br />
+<p>
+1. Practice schedule posted on Facebook and this website.  <br />
+2. Upcoming events posted on Facebook and this website. <br />
 3a. Study the rulebook (I recommend AT LEAST chapters 5 and 6 as they have the most player-based
 rules versus game rules, but you really should read the whole thing at least once). <br />
-3b. The rulebook can be kind of intimidating, I know. So to help, we have these handy rulebook flash
-cards! Quizlet also has an app so you can read them on the go. :) <br />
+3b. The rulebook can be kind of intimidating, we know. So to help, we have rulebook flash
+cards. Quizlet also has an app so you can read them on the go. <br />
 4. Take the Assistant Referee test. Once you pass, take the Snitch Referee test. On the left side, Tier 5 and 4. <br />
 5. Like our public page and invite all your friends. <br />
 </p>
 
 <hr> <br />
 <h2>Helpful Links</h2>
-<a href="http://www.usquidditch.org/files/USQ_Rulebook_10.pdf">Quidditch Rule Book</a> <br />
+<a href="http://www.usquidditch.org/files/USQ_Rulebook_10.pdf">Quidditch Rule Book</a>
 <a href="https://quizlet.com/themunchkym/folders/us-quidditch-rulebook-tenth-edition">Rule Book Flash Cards</a> <br />
 <a href="https://www.facebook.com/groups/316777608428869/">Team Facebook Page</a> <br />
-<a href="https://www.facebook.com/BSAQuidditch">Public Facebook Page</a> <br />
+<a href="https://www.facebook.com/BSAQuidditch">Public Facebook Page</a> 
 <a href="https://docs.google.com/document/d/1W2YQfNEA0jK3WAkl1-PMgz3YjF8SLl_c9G7qM8rZh-U/edit?usp=sharing">Jersey Numbers</a> <br />
 <hr>
 
