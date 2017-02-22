7c0b7c9cff95b14e21786298121ec265d4969290
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-21 19:44:05 -0700

    adding photo gallery home page pt5

--- index.php
+++ index.php
@@ -14,28 +14,28 @@
     </ul>
   </div>
 
-  <div class="img">
+  <div class="imgLeft">
   <a target="_blank" href="images/mattSnitchCatch.jpg">
     <img src="images/mattSnitchCatch.jpg" alt="McKracken Snitch Catch" width="600" height="400">
   </a>
   <div class="desc">Matt McKracken pulls the snitch to wint the game </div>
 </div>
 
-<div class="img">
+<div class="imgLeft">
   <a target="_blank" href="images/bryanDunk.jpg">
     <img src="images/bryanDunk.jpg" alt="Bryan Dunking on the opponents" width="600" height="400">
   </a>
   <div class="desc">Bryan Bixler dunks on the oposing team</div>
 </div>
 
-<div class="img">
+<div class="imgRight">
   <a target="_blank" href="images/boysCelebrate.jpg">
     <img src="images/boysCelebrate.jpg" alt="Guys celebrating the tournament win" width="600" height="400">
   </a>
   <div class="desc">The guys celebrating the tournament win </div>
 </div>
 
-<div class="img">
+<div class="imgRight">
   <a target="_blank" href="images/vandyDunk.jpg">
     <img src="images/vandyDunk.jpg" alt="Nick dunks on female chaser" width="600" height="400">
   </a>
