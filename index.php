b024cb579dabdba86c20f4347454af23eb35b7a7
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-22 00:05:38 -0700

    retry social media footer pt3

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 
   <div id="nav">
     <ul>
-      <li><a class=active href="index.php">Home</a></li>
-      <li><a href="roster.php">Roster</a></li>
-      <li><a href="upcoming.php">Upcoming Tournaments</a></li>
-      <li><a href="videos.php">Videos</a></li>
-      <li><a href="practices.php">Practices</a></li>
-      <li><a href="announcements.php">Announcements</a></li>
+      <li><nav.a class=active href="index.php">Home</a></li>
+      <li><nav.a href="roster.php">Roster</a></li>
+      <li><nav.a href="upcoming.php">Upcoming Tournaments</a></li>
+      <li><nav.a href="videos.php">Videos</a></li>
+      <li><nav.a href="practices.php">Practices</a></li>
+      <li><nav.a href="announcements.php">Announcements</a></li>
     </ul>
   </div>
 
