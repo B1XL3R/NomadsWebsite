51ad3dfe46665133068a9e4fa424a79c55dbd0ad
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-21 23:45:58 -0700

    removing social media footer pt1

--- footer.php
+++ footer.php
@@ -1,16 +1,4 @@
 <img id="footer" src="images/footer.jpg" />
 
-<div class="footer-social-icons">
-    <h4 class="_14">Follow us on</h4>
-    <footer.ul class="social-icons">
-        <li><a href="" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-rss"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
-        <li><a href="" class="social-icon"> <i class="fa fa-google-plus"></i></a></li>
-    </footer.ul>
-</div>
-
   </body>
 </html>
