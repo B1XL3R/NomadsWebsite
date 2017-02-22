05ee948d81b04b4f3951aa0dd446a9b7914dc4cb
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-02-21 20:12:24 -0700

    adding table to upcoming pt1

--- roster.php
+++ roster.php
@@ -16,7 +16,7 @@
 
   <div class="imgRoster">
   <a target="_blank" href="images/roster.jpg">
-    <img src="images/roster.jpg" alt="Abraxans 2017 Roster" width=100% height=auto>
+    <img src="images/roster.jpg" alt="Abraxans 2017 Roster" width=1000px height=auto>
   </a>
   <div class="desc">Back Row From left to right: Dylan Shilling, Matt McKracken, Bryan Bixler, Nick Hall,
     Alex Johnson, Jessie Gibson, Kevin Kelly, Estefani De La Rosa. Front Row left to right: Nick Vandy, Jami Wyman,
