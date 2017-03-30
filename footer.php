ed9c5a0de19ab41a9eaa009972d7e570989e4aa7
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-03-30 11:16:50 -0600

    adding login page pt1

--- footer.php
+++ footer.php
@@ -3,37 +3,7 @@
         <div>
             <p>- Boise State abraxans -</p>
             <br />
-
-
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-<div id="id01" class="modal">
-
-  <form class="modal-content animate" action="/action_page.php">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-
-      <button type="submit">Login</button>
-      <input type="checkbox" checked="checked"> Remember me
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-
-<a href="https://www.facebook.com/BSAQuidditch"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+              <a href="https://www.facebook.com/BSAQuidditch"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
 	            <a href="https://twitter.com/bsaquidditch"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
 	            <a href="mailto:abraxans@boisestate.edu.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
 </div>
@@ -43,21 +13,6 @@
 <br />
 
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-
-
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
-
 
   </body>
 </html>
