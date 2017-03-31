3fea3933b9d9cbf602b213b94c612ab9900f53a5
Bryan Bixler <bryanbixler@u.boisestate.edu>
2017-03-31 13:45:27 -0600

    adding login page pt2

--- login.php
+++ login.php
@@ -2,6 +2,17 @@
   include('header.php');
 ?>
 
+<div id="nav">
+  <ul>
+    <li><a class=active href="index.php">Home</a></li>
+    <li><a href="roster.php">Roster</a></li>
+    <li><a href="upcoming.php">Upcoming Tournaments</a></li>
+    <li><a href="videos.php">Videos</a></li>
+    <li><a href="practices.php">Practices</a></li>
+    <li><a href="announcements.php">Announcements</a></li>
+  </ul>
+</div>
+
 <h2>Login Form</h2>
 
 <form action="/action_page.php">
