bf257da39ace4e36ebe7ec74082f0b9e3394961e
Bryan Bixler <bryanbixler@u.boisestate.edu>
2018-04-02 16:04:11 -0700

    adding login page to site

--- login.php
+++ login.php
@@ -4,12 +4,13 @@
 
 <div id="nav">
   <ul>
-    <li><a class=active href="index.php">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li><a href="roster.php">Roster</a></li>
     <li><a href="upcoming.php">Upcoming Tournaments</a></li>
     <li><a href="videos.php">Videos</a></li>
     <li><a href="practices.php">Practices</a></li>
     <li><a href="announcements.php">Announcements</a></li>
+    <li><a class=active href="login.php">Login</a></li>
   </ul>
 </div>
 
